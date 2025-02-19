924398cfc80c66afe489fa5ffe6ffadbe3516011
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-02-20 00:41:36 +0700

    v3
    update enemy

---
+++ Assets/scripting/EnemyPhai_Trai.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Enemy:MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        rb.linearVelocityX = -speed;
+    }
+}
