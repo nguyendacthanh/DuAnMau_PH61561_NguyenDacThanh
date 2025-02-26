6073841ccfef7972238b35bb55c4c88417ec0ae1
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-02-26 23:11:33 +0700

    reVersion1
    reMAP2

---
+++ Assets/scripting/MAP2C#/Map2BackGround.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Map2:MonoBehaviour
+{
+    public float speed = 10f; 
+
+    void Update()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+  
+}
