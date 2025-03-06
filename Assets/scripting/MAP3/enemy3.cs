4c36a0cc00476940ee48f30e6e5f4f6966b96174
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-03-06 22:36:26 +0700

    v11.1 update map 3

---
+++ Assets/scripting/MAP3/enemy3.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class enemy3 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+
+    //phan di chuyen
+    public float moveSpeed = 15f;
+    public float timePerMove = 2f;
+    //public GameObject eventChanceMove;
+    private bool isMovingRandom = false;
+
+    //phan auto ban
+    private Animator animator;
+    public GameObject bullet;
+    public Transform vitri;
+    public bool canfire = true;
+
+
+    public int damage = 1;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.linearVelocity = Vector2.down * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HP hp = collision.GetComponent<HP>();
+
+        if (collision.gameObject.CompareTag("eventChanceMove") && !isMovingRandom)
+        {
+            isMovingRandom = true;
+            StartCoroutine(SetRandomMove());
+            StartCoroutine(setfirer());
+        }
+        if (collision.CompareTag("main"))
+        {
+
+            hp.TakeDamage(damage);
+            animator.SetTrigger("death");
+            Destroy(gameObject, 0.5f);
+            Die();
+        }
+
+    }
+
+    IEnumerator SetRandomMove()
+    {
+        rb.linearVelocity = Vector2.zero;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(timePerMove);
+
+            float x = Random.Range(-50, 0);
+            float y = Random.Range(-8, -7);
+            Vector2 targetPosition = new Vector2(Random.Range(-65, -10), Random.Range(-11, -9));
+
+            Vector2 moveDirection = (targetPosition - rb.position).normalized;
+
+            rb.linearVelocity = moveDirection * moveSpeed;
+        }
+    }
+    IEnumerator setfirer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.5f);
+            animator.SetTrigger("enemy_atk");
+            Instantiate(bullet, vitri.position, Quaternion.Euler(0, 0, 270));
+
+        }
+
+    }
+    public enemySpawnEvent spawnManager;
+
+    void OnDestroy()
+    {
+        if (spawnManager != null)
+        {
+            spawnManager.DecreaseEnemyCount();
+        }
+    }
+
+
+    // đếm số lượng quái bị giết để theo dõi tiến trình hoàn thành nhiệm vụ, check điều kiện thắng
+    private void Die()
+    {
+        CheckWinning winManager = FindFirstObjectByType<CheckWinning>();
+        EnemyKillCount counter = FindAnyObjectByType<EnemyKillCount>();
+        if (counter != null)
+        {
+            counter.IncreaseKillCount();
+        }
+        if (winManager != null)
+        {
+            winManager.EnemyDefeated();
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripting/MAP3/main3.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class main3 : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private float moveSpeed = 20f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public GameObject bulletPrefab;
+    public Transform vitri;
+    public float fireRate = 0.2f;
+    private bool canFire = true;
+    Animator animator;
+    public int damage = 1;
+
+    public int checkHeal = 1;
+    private HP healing;
+    public GameObject winPanel;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        healing = GetComponent<HP>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (canFire && Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Fire());
+
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            HP hP = GetComponent<HP>();
+            if (hP != null)
+            {
+                hP.TakeDamage(damage);
+            }
+
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("HP"))
+        {
+            Heal();
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("win"))
+        {
+            winPanel.SetActive(true);
+        }
+
+    }
+
+
+
+    IEnumerator Fire()
+    {
+        canFire = false;
+        animator.SetTrigger("atk");
+        Instantiate(bulletPrefab, vitri.position, Quaternion.identity);
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+
+    private void Heal()
+    {
+        if (healing != null && healing.currentHP < healing.maxHP)
+        {
+            healing.currentHP += 1; // Tăng 1 HP
+            healing.UpdateHPUI(); // Cập nhật hiển thị HP
+        }
+    }
+}
