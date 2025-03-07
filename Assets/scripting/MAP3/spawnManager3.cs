af3c16e3b727df0f9263606f6853128da8c6e9e8
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-03-07 15:34:56 +0700

    v13 all done

--- Assets/scripting/MAP3/spawnManager3.cs
+++ Assets/scripting/MAP3/spawnManager3.cs
@@ -19,6 +19,8 @@ public class spawnManager3 :MonoBehaviour
                 float getRandomY = Random.Range(-30f, -4f);
                 Vector3 viTriRandom_spawn = new Vector3(27, getRandomY, 1);
                 GameObject newEnemy = Instantiate(enemy, viTriRandom_spawn, Quaternion.identity);
+                GameObject newEnemy2 = Instantiate(enemy, viTriRandom_spawn, Quaternion.identity);
+                GameObject newEnemy3 = Instantiate(enemy, viTriRandom_spawn, Quaternion.identity);
                 count++;
                 newEnemy.GetComponent<enemy3>().spawnManager = this;
 
